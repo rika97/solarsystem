3a9974e359421084f9a438d731ee9e4896ec4526
Rikako <15hatoyar@gmail.com>
2018-01-21 16:25:42 -0800

    Configured initial conditions. Displayed date.

--- solarsystem.py
+++ solarsystem.py
@@ -1,13 +1,17 @@
 import pygame
+from datetime import datetime
+
 pygame.init()
 screen_width = 800
 screen_height = 600
 DISPLAYSURF = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Solar System Simulator")
 screen = pygame.display.get_surface()
+sidereal_year = 365.25636
+tropical_year = 365.24219
 
 G = 6.6741 * 10**(-11)
-t = 0
+t = 1167882180
 sun = {
     'mass': 1.9886 * 10**30,
     'radius': 695508000,
@@ -21,30 +25,32 @@ sun = {
 earth = {
     'mass': 5.9722 * 10**24,
     'radius': 6371000 * 5,
-    'x': sun['x'] + 1.496 * 10**11,
+    'x': sun['x'] + 147098291000,
     'y': sun['y'],
     'z': sun['z'],
     'vx': sun['vx'],
-    'vy': sun['vy'] + 2.9786 * 10**4,
+    'vy': sun['vy'] + 30280,
     'vz': sun['vz'],
 }
 scale = 10**9
 
 
 def print_status():
-    print("Time:", t)
-    print(
-        "  Earth position:",
-        earth['x'],
-        earth['y'],
-        earth['z'],
-    )
-    print(
-        "  Sun position:",
-        sun['x'],
-        sun['y'],
-        sun['z'],
-    )
+    dt = datetime.fromtimestamp(t)
+    date_string = dt.strftime('%Y-%m-%d %H:%M:%S')
+    print("\rTime: " + date_string, end='')
+    # print(
+    #     "  Earth position:",
+    #     earth['x'],
+    #     earth['y'],
+    #     earth['z'],
+    # )
+    # print(
+    #     "  Sun position:",
+    #     sun['x'],
+    #     sun['y'],
+    #     sun['z'],
+    # )
 
 
 def do_step(duration):
@@ -79,6 +85,8 @@ dur = float(input("Enter duration: "))
 steps = int(input("Enter number of steps: "))
 
 print_status()
+
 while steps > 0:
     do_step(dur)
     steps -= 1
+    print_status()
